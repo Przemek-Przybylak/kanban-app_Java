585b9eb7254fb123107ad7fc0a3090fa6fbef01a
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-06 11:54:50 +0100

    update project name app file

--- src/main/java/com/example/kanban/KanbanApplication.java
+++ src/main/java/com/example/kanban/KanbanApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.kanban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class KanbanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(KanbanApplication.class, args);
 	}
 
 }
