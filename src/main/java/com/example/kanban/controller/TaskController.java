32e9decb3f1f13da3e69867184ef019c9055d609
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-07 11:04:58 +0100

    fetch task implmenets in kontorller

--- src/main/java/com/example/kanban/controller/TaskController.java
+++ src/main/java/com/example/kanban/controller/TaskController.java
@@ -2,25 +2,34 @@ package com.example.kanban.controller;
 
 import com.example.kanban.model.Task;
 import com.example.kanban.model.TaskRepository;
+import com.example.kanban.service.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-
-import java.awt.*;
 
 @RestController
 public class TaskController {
     private final TaskRepository taskRepository;
+    private final TaskService taskService;
 
-    public TaskController(TaskRepository taskRepository) {
+    public TaskController(TaskRepository taskRepository, TaskService taskService) {
         this.taskRepository = taskRepository;
+        this.taskService = taskService;
     }
 
     @GetMapping("tasks")
-    public ResponseEntity<List<Task>> getALLTasks() {
+    public ResponseEntity<List<Task>> getAllTasks() {
         List<Task> allTasks = taskRepository.findAll();
 
         return ResponseEntity.ok(allTasks);
+    }
+
+    @GetMapping("tasks/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable String id) {
+        Task task = taskService.getTask(id);
+
+        return ResponseEntity.ok(task);
     }
 }
