f3f318838b662f1e230683e82c922db2175a2ffb
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-07 11:05:42 +0100

    added interface for task service

---
+++ src/main/java/com/example/kanban/service/TaskServiceInterface.java
@@ -0,0 +1,13 @@
+package com.example.kanban.service;
+
+import com.example.kanban.model.Task;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface TaskServiceInterface {
+    List<Task> getAllTasks();
+
+    @Transactional(readOnly = true)
+    Task getTask(String id);
+}
