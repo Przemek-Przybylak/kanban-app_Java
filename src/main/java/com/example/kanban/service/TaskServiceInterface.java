7a2052d9e5630017959b3958e34a24c1862e1aae
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-08 20:02:17 +0100

    added edit, add, delete methods into Task service interface

--- src/main/java/com/example/kanban/service/TaskServiceInterface.java
+++ src/main/java/com/example/kanban/service/TaskServiceInterface.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface TaskServiceInterface {
+    @Transactional(readOnly = true)
     List<Task> getAllTasks();
 
     @Transactional(readOnly = true)
@@ -13,4 +14,13 @@ public interface TaskServiceInterface {
 
     @Transactional
     Task addTask(String projectId, Task task);
+
+    @Transactional
+    Task editTask(String projectId, Task task);
+
+    @Transactional
+    Task editPartialTask(String taskId, Task task);
+
+    @Transactional
+    void deleteTask(String id);
 }
