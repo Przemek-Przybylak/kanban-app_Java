0a8992dbc64af1949e6eb0d53d8902e7c95ffb11
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-08 05:34:33 +0100

    post task implementation in TaskCService!

--- src/main/java/com/example/kanban/service/TaskService.java
+++ src/main/java/com/example/kanban/service/TaskService.java
@@ -1,5 +1,7 @@
 package com.example.kanban.service;
 
+import com.example.kanban.model.Project;
+import com.example.kanban.model.ProjectRepository;
 import com.example.kanban.model.Task;
 import com.example.kanban.model.TaskRepository;
 import org.springframework.http.HttpStatus;
@@ -12,13 +14,15 @@ import java.util.List;
 @Service
 public class TaskService implements TaskServiceInterface{
     private final TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
 
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, ProjectRepository projectRepository) {
         this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
     }
 
-    @Override
     @Transactional(readOnly = true)
+    @Override
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
@@ -28,5 +32,16 @@ public class TaskService implements TaskServiceInterface{
     public Task getTask(String id) {
         return taskRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found"));
+    }
+
+    @Transactional
+    @Override
+    public Task addTask(String projectId, Task task) {
+        Project project = projectRepository
+                .findById(projectId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Project not found"));
+
+        task.setProject(project);
+        return taskRepository.save(task);
     }
 }
