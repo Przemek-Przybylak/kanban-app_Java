596630a28922aadae606140f09a9c61cc9564969
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-09 18:52:24 +0100

    implement interface for ProjectService

---
+++ src/main/java/com/example/kanban/service/ProjectServiceInterface.java
@@ -0,0 +1,15 @@
+package com.example.kanban.service;
+
+import com.example.kanban.model.Project;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface ProjectServiceInterface {
+
+    @Transactional(readOnly = true)
+    List<Project> getAllProjects();
+
+    @Transactional(readOnly = true)
+    Project getProject(String id);
+}
