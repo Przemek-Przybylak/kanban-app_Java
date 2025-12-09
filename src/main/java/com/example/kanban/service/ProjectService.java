d7adf28a54bacb0542a83519cd301da48bf2039c
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-09 18:51:40 +0100

    Added service, and get mapping logic into

---
+++ src/main/java/com/example/kanban/service/ProjectService.java
@@ -0,0 +1,33 @@
+package com.example.kanban.service;
+
+import com.example.kanban.model.Project;
+import com.example.kanban.model.ProjectRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class ProjectService implements ProjectServiceInterface{
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Project getProject(String id) {
+        return projectRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found"));
+    }
+}
+
