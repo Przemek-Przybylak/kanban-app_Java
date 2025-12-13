991676bcaae9277d81f82ca58dcea3b4b8e4b320
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-13 09:58:07 +0100

    added Task response dto mapper

--- src/main/java/com/example/kanban/DTO/Mapper.java
+++ src/main/java/com/example/kanban/DTO/Mapper.java
@@ -1,10 +1,11 @@
 package com.example.kanban.DTO;
 
 import com.example.kanban.model.Project;
+import com.example.kanban.model.Task;
 
 public class Mapper {
 
-    public static ProjectResponseDto toTdo(Project entity) {
+    public static ProjectResponseDto toDto(Project entity) {
         return new ProjectResponseDto(
                 entity.getId(),
                 entity.getTitle(),
@@ -13,8 +14,22 @@ public class Mapper {
                 entity.getCreatedAt(),
                 entity.getUpdatedAt(),
                 entity.getTasks().stream()
-                        .map(task -> new shortTasksDto(task.getId(),task.getTitle()))
+                        .map(task -> new shortTasksDto(task.getId(), task.getTitle()))
                         .toList()
+        );
+    }
+
+    public static TaskResponseDto toDto(Task entity) {
+        return new TaskResponseDto(
+                entity.getId(),
+                entity.getTitle(),
+                entity.getDescription(),
+                entity.getStatus(),
+                entity.getMembers(),
+                entity.getApprovedBy(),
+                entity.getDueDate(),
+                entity.getCreatedAt(),
+                new shortProjectDto(entity.getProject().getId(), entity.getProject().getTitle())
         );
     }
 }
