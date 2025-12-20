00dc16987971f1ee5c67a09a8b297e9091c6b8d5
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-20 13:43:07 +0100

    added all Project service tests

--- src/test/java/com/example/demo/service/ProjectServiceTest.java
+++ src/test/java/com/example/demo/service/ProjectServiceTest.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.kanban.model.Project;
 import com.example.kanban.model.ProjectRepository;
+import com.example.kanban.model.Task;
 import com.example.kanban.model.TaskRepository;
 import com.example.kanban.service.ProjectService;
 import org.junit.jupiter.api.Test;
@@ -9,14 +10,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ProjectServiceTest {
@@ -49,6 +53,52 @@ public class ProjectServiceTest {
                 .thenReturn(Optional.empty());
 
         ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> projectService.getProject("123"));
+
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+    }
+
+    @Test
+    void shouldEditProjectPartially() {
+        Task task = new Task();
+        task.setId("t1");
+
+        Project existingProject = new Project();
+        existingProject.setId("123");
+        existingProject.setTitle("example");
+        existingProject.setTasks(List.of(task));
+
+        when(projectRepository.findById("123"))
+                .thenReturn(Optional.of(existingProject));
+
+        when(projectRepository.save(any(Project.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        Project changedProject = new Project();
+        changedProject.setTitle("example2");
+
+        Project result = projectService.editPartialProject("123", changedProject);
+
+        assertEquals("example2", result.getTitle());
+    }
+
+    @Test
+    void shouldDeleteProject() {
+        Project project = new Project();
+        project.setId("123");
+
+        when(projectRepository.findById("123"))
+                .thenReturn(Optional.of(project));
+
+        projectService.deleteProject("123");
+
+        verify(projectRepository).delete(project);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenProjectToDeleteNotExist() {
+        ResponseStatusException exception =
+                assertThrows(ResponseStatusException.class,
+                        () -> projectService.deleteProject("123"));
 
         assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
     }
