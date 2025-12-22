16d80a5d0cf10a69be4934f8c5bc58504599ca18
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-22 05:26:37 +0100

    User model added

---
+++ src/main/java/com/example/kanban/user/model/User.java
@@ -0,0 +1,24 @@
+package com.example.kanban.user.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(length = 36, name = "user_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+   @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
