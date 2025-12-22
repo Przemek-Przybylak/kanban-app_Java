80e40b4a2f927f9b54d29d4135f1823bec04227b
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-22 19:34:04 +0100

    register implement in user service

--- src/main/java/com/example/kanban/user/service/UserService.java
+++ src/main/java/com/example/kanban/user/service/UserService.java
@@ -1,14 +1,36 @@
 package com.example.kanban.user.service;
 
 import com.example.kanban.user.dto.RegisterRequestDto;
+import com.example.kanban.user.model.Role;
+import com.example.kanban.user.model.User;
+import com.example.kanban.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Transactional
-    public void register(RegisterRequestDto requestDto) {
+    public User register(RegisterRequestDto requestDto) {
+        Optional<User> isUsernameUse = userRepository.findByUsername(requestDto.getLogin());
+        if (isUsernameUse.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Username is already used");
+        }
 
+        User user = new User();
+        user.setRole(Role.USER);
+        user.setUsername(requestDto.getLogin());
+        user.setPassword(requestDto.getPassword());
+
+        return userRepository.save(user);
     }
 }
