590e11cf3a4010ac4937d7037737fbfec1d48abf
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-27 10:09:13 +0100

    validate user input data when login

--- src/main/java/com/example/kanban/user/dto/RegisterRequestDto.java
+++ src/main/java/com/example/kanban/user/dto/RegisterRequestDto.java
@@ -1,4 +1,10 @@
 package com.example.kanban.user.dto;
 
-public record RegisterRequestDto(String username, String password) {
+import jakarta.validation.constraints.NotBlank;
+
+public record RegisterRequestDto(
+        @NotBlank(message = "Username is required")
+        String username,
+        @NotBlank(message = "Password cannot be empty")
+        String password) {
 }
