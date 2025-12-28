2f46d1e82b20ffc123fbbfa6fa81732fc2d356a8
Przemek <przemyslaw.przybylak2@gmail.com>
2025-12-28 11:54:05 +0100

    added find task by project function in TaskRepository component

--- src/main/java/com/example/kanban/model/TaskRepository.java
+++ src/main/java/com/example/kanban/model/TaskRepository.java
@@ -2,5 +2,8 @@ package com.example.kanban.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, String> {
+    List<Task> findByProjectId(String projectId);
 }
